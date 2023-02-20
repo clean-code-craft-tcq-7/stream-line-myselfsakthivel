1d21615bb8edc7eb471b56175c4b9770bfeefdae
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2023-02-20 13:39:59 +0530

    Update sender.test.js

--- test/sender.test.js
+++ test/sender.test.js
@@ -1,1 +1,26 @@
+const { expect } = require('chai');
+const sender = require('../sender');
 
+describe('randomValueGenerator', () => {
+    it('should exists', () => {
+        expect(sender.randomValueGenerator).to.exist;
+    });
+});
+
+describe('temperatureSensor', () => {
+    it('should exists', () => {
+        expect(sender.temperatureSensor).to.exist;
+    });
+});
+
+describe('socSensor', () => {
+    it('should exists', () => {
+        expect(sender.socSensor).to.exist;
+    });
+});
+
+describe('sensorStatistics', () => {
+    it('should exists', () => {
+        expect(sender.sensorStatistics).to.exist;
+    });
+});
