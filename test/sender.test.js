c0614fa343f644128cb40ad8d48306f0fd5aa127
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2023-02-20 13:44:00 +0530

    Update sender.test.js

--- test/sender.test.js
+++ test/sender.test.js
@@ -2,25 +2,42 @@ const { expect } = require('chai');
 const sender = require('../sender');
 
 describe('randomValueGenerator', () => {
-    it('should exists', () => {
+    it('Should exists', () => {
         expect(sender.randomValueGenerator).to.exist;
+    });
+    
+    let list = sender.randomValueGenerator(1, 10, 5);
+    
+    it('Should return randomValue', () => {
+        expect(list.length).to.deep.equals(5);
+    });
+    
+    it('Should return temperature sensor exact value count', () => {
+        expect(list.length).to.deep.equals(5);
+    });
+    
+    it('Should return temperature sensor random value between the min and max value given', () => {
+        let minValue = Math.min(...list);
+        let maxValue = Math.max(...list);
+        expect(minValue).to.be.above(0)
+        expect(maxValue).to.be.below(10)
     });
 });
 
 describe('temperatureSensor', () => {
-    it('should exists', () => {
+    it('Should exists', () => {
         expect(sender.temperatureSensor).to.exist;
     });
 });
 
 describe('socSensor', () => {
-    it('should exists', () => {
+    it('Should exists', () => {
         expect(sender.socSensor).to.exist;
     });
 });
 
 describe('sensorStatistics', () => {
-    it('should exists', () => {
+    it('Should exists', () => {
         expect(sender.sensorStatistics).to.exist;
     });
 });
