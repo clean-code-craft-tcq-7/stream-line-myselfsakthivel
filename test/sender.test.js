6cc3bb0ccaddcee0dadf2871fe22c46d3b2d595a
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2023-02-20 16:05:17 +0530

    Update sender.test.js

--- test/sender.test.js
+++ test/sender.test.js
@@ -15,8 +15,8 @@ describe('randomDataGenerator', () => {
     it('Should return correct values inside given min max values', () => {
         let minValue = Math.min(...listToTest);
         let maxValue = Math.max(...listToTest);
-        expect(minValue).to.be.above(0)
-        expect(maxValue).to.be.below(10)
+        expect(minValue).to.be.at.most(0)
+        expect(maxValue).to.be.at.least(10)
     });
 });
 
@@ -34,8 +34,8 @@ describe('simulateTemperatureSensor', () => {
     it('Should return correct values inside given min max values', () => {
         let minValue = Math.min(...listToTest);
         let maxValue = Math.max(...listToTest);
-        expect(minValue).to.be.above(0)
-        expect(maxValue).to.be.below(125)
+        expect(minValue).to.be.at.most(0)
+        expect(maxValue).to.be.at.least(125)
     });
 });
 
@@ -53,8 +53,8 @@ describe('simulateSocSensor', () => {
     it('Should return correct values inside given min max values', () => {
         let minValue = Math.min(...listToTest);
         let maxValue = Math.max(...listToTest);
-        expect(minValue).to.be.above(20)
-        expect(maxValue).to.be.below(50)
+        expect(minValue).to.be.at.most(20)
+        expect(maxValue).to.be.at.least(50)
     });
 });
 
