066af458e6f44376f5597606dfb8d27530ed06f9
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2023-02-20 15:36:12 +0530

    Update sender.test.js

--- test/sender.test.js
+++ test/sender.test.js
@@ -24,16 +24,48 @@ describe('simulateTemperatureSensor', () => {
     it('Should exists', () => {
         expect(sender.simulateTemperatureSensor).to.exist;
     });
+    
+    let listToTest = sender.simulateTemperatureSensor(20);
+    
+    it('Should return correct range', () => {
+        expect(listToTest.length).to.deep.equals(20);
+    });
+    
+    it('Should return correct values inside given min max values', () => {
+        let minValue = Math.min(...listToTest);
+        let maxValue = Math.max(...listToTest);
+        expect(minValue).to.be.above(0)
+        expect(maxValue).to.be.below(125)
+    });
 });
 
 describe('simulateSocSensor', () => {
     it('Should exists', () => {
         expect(sender.simulateSocSensor).to.exist;
     });
+    
+    let listToTest = sender.simulateSocSensor(30);
+    
+    it('Should return correct range', () => {
+        expect(listToTest.length).to.deep.equals(30);
+    });
+    
+    it('Should return correct values inside given min max values', () => {
+        let minValue = Math.min(...listToTest);
+        let maxValue = Math.max(...listToTest);
+        expect(minValue).to.be.above(20)
+        expect(maxValue).to.be.below(50)
+    });
 });
 
 describe('prepareStreamData', () => {
     it('Should exists', () => {
         expect(sender.prepareStreamData).to.exist;
+    });
+    
+    let listToTest = sender.prepareStreamData(10);
+    
+    it('Should return correct range', () => {
+        expect(listToTest.length).to.deep.equals(10);
     });
 });
